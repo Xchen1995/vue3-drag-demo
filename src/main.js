e70b38a3bab379771e2ca4f9cefd630c07aabf72
Xchen1995 <studentchenxu@163.com>
2021-07-30 13:40:56 +0800

    增加element

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from './store';
 
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
 const app = createApp(App);
 
+app.use(ElementPlus);
 app.use(router);
 app.use(store);
 app.mount("#app");
