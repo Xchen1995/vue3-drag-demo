0cc7db138682ff779c321d328642aa21bc1f1817
Xchen1995 <studentchenxu@163.com>
2021-08-02 18:39:45 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import '@/custom-component'; // 注册自定义组件
 
 import ElementPlus from "element-plus";
 import "element-plus/lib/theme-chalk/index.css";
